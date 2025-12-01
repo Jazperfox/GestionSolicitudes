5ff847a97ab2c49d47af6ce0f63b888e6887a5dc
Jason <ajasonkristof@hotmail.com>
2025-12-01 04:15:32 -0600

    subir

---
+++ MoonPhase/MoonPhase/src/main/java/com/example/MoonPhase/MoonPhaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.MoonPhase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonPhaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoonPhaseApplication.class, args);
+	}
+
+}
